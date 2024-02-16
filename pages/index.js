353f3e012f7a1684763b06d076afa587fca93ab0
JuiHung Kao <rayhungkao@gmail.com>
2024-02-16 16:18:44 +0800

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Home page</h1>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/hw1">hw1</Link>
-            </li>
-            <li>
-              <Link href="/hw2">hw2</Link>
-            </li>
-            <li>
-              <Link href="/hw3">hw3</Link>
-            </li>
-            <li>
-              <Link href="/hw4">hw4</Link>
-            </li>
-            <li>
-              <Link href="/hw5">hw5</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <h2>Merge hw1~hw5 via Next.js pages router</h2>
     </div>
   );
 }
