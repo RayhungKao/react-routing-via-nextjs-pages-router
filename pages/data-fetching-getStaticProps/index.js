96b9aa7670e12c6cfff5e4703a40945612678850
JuiHung Kao <rayhungkao@gmail.com>
2024-02-19 18:31:43 +0800

    add routes for getStaticProps, getServerSideProps with fake data

---
+++ pages/data-fetching-getStaticProps/index.js
@@ -0,0 +1,28 @@
+import User from '../../components/user';
+
+export default function UserListFromGetStaticProps({ users }) {
+  return (
+    <div>
+      <h1>User</h1>
+      <h2>fake data from jsonplaceholder</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <User user={user}/>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
